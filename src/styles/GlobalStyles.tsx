5d61f46c26265bda38e2777ee932f50663a1d5e4
Anderson Silva <andeerson0017@gmail.com>
2020-10-30 11:10:50 -0300

    chore: adicionando styled-components e criando GlobalStyles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components'
+export default createGlobalStyle`
+* {
+    font-feature-settings: "liga" on, "calt" on;
+  -webkit-font-feature-settings: "liga" on, "calt" on;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+  font-family: 'Montserrat', sans-serif;
+  text-decoration: none;
+  list-style-type: none;
+}
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ol,
+ul {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  font-weight: normal;
+}
+ol,
+ul {
+  list-style: none;
+}
+#root, body, html {
+  height: 100%;
+  width: 100%;
+}
+`
