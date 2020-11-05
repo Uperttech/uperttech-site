560152bc86f58279f6111f05c87c5545d82a3286
Anderson Silva <andeerson0017@gmail.com>
2020-11-05 16:07:56 -0300

    refactor: refatorando ActionButton

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,19 +1,57 @@
-import React from "react";
-import styled from "styled-components";
+import styled, { css, keyframes } from 'styled-components'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
-export const ActionButton: React.FC<{ text: string; width?: string }> = ({
-  text,
-  width,
-}) => {
-  return <StyledActionButton style={{ width }}>{text}</StyledActionButton>;
-};
+type ActionButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  primary?: boolean
+  loading?: boolean
+  text: string
+}
 
-const StyledActionButton = styled.div`
-  text-align: center;
-  font-weight: 700;
-  font-size: 15px;
+export const ActionButton: React.FC<ActionButtonProps> = props => (
+  <StyledActionButton type="button" {...props}>
+    {props.loading ? <AiOutlineLoading3Quarters size={20} color="#FFF" /> : ''}
+    {props.loading ? '' : props.text}
+  </StyledActionButton>
+)
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const StyledActionButton = styled.button<ActionButtonProps>`
+  background-color: ${props => (props.primary ? '#4C71FB' : '#c2c2c2')};
+  display: block;
   color: #fff;
-  background-color: #4e71fb;
-  padding: 15px 28px;
-  box-shadow: #4e71fb5e 0 6px 9px;
-`;
+  font-weight: 500;
+  font-family: inherit;
+  border: 0;
+  padding: 15px 25px;
+  width: 100%;
+  max-width: 420px;
+  height: 50px;
+  text-align: center;
+  font-size: 17px;
+  border-radius: 3px;
+  cursor: pointer;
+  opacity: 0.9;
+  transition: all ease-in-out 0.2s;
+
+  :hover {
+    opacity: 1;
+  }
+
+  ${props =>
+    props.loading &&
+    css`
+      cursor: not-allowed;
+      opacity: 0.8;
+      > svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
+`
