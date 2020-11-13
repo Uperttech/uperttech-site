692b4a9bd8dc6d004303fc693bbc5f84f471529f
Anderson Silva <andeerson0017@gmail.com>
2020-11-13 11:33:40 -0300

    chore: adicionando AppProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import GlobalStyles from '../styles/GlobalStyles'
 import { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import AppProvider from '~/providers/AppProvider'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
       <ToastContainer autoClose={3000} />
     </>
   )
