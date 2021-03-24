10306af8a27824986bab1d8098fc2baa923a4246
Lucas Barbosa <llbarbosasc@gmail.com>
2021-03-24 13:07:43 -0400

    fix: Google Forms CORS

--- src/util/googleFormsAPI.ts
+++ src/util/googleFormsAPI.ts
@@ -1,26 +1,30 @@
 export const googleFormsAPI = {
   sendContact: async (data: FormData): Promise<void> => {
     const response = await fetch(
-      process.env.NEXT_PUBLIC_CONTACTFORM_URL as string,
+      'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdGQNmUGUbGXgPQAAqIAbxj6JYeLo0ca8ScL5TO0-XiJBgSPw/formResponse',
       {
         method: 'POST',
-        body: data
+        body: data,
+        mode: 'no-cors'
       }
     )
-    if (!response.ok) {
+
+    if (!response.ok && response.type !== 'opaque') {
       throw new Error('Não foi possível enviar sua mensagem, tente novamente')
     }
   },
 
   sendBudget: async (data: FormData): Promise<void> => {
     const response = await fetch(
-      process.env.NEXT_PUBLIC_BUDGETFORM_URL as string,
+      'https://docs.google.com/forms/u/0/d/e/1FAIpQLSf-Up_irXDJKFfL1b8k3_G9NalEu1TZAonIyjYDEqRrYU-Cow/formResponse',
       {
         method: 'POST',
-        body: data
+        body: data,
+        mode: 'no-cors'
       }
     )
-    if (!response.ok) {
+
+    if (!response.ok && response.type !== 'opaque') {
       throw new Error('Não foi possível enviar seu orçamento, tente novamente')
     }
   }
