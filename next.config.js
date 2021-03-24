3371f69e8759039ae3feda0591fd86288d0234e5
Lucas Barbosa <llbarbosasc@gmail.com>
2021-03-24 12:35:17 -0400

    fix: Envio dos formulários em produção

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   async rewrites() {
-      return [
-        {
-          source: '/api/contact_form',
-          destination: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdGQNmUGUbGXgPQAAqIAbxj6JYeLo0ca8ScL5TO0-XiJBgSPw/formResponse',
-        },
-        {
-          source: '/api/budget_form',
-          destination: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSf-Up_irXDJKFfL1b8k3_G9NalEu1TZAonIyjYDEqRrYU-Cow/formResponse',
-        },
-      ]
-    },
+    return [
+      {
+        source: '/api/contact_form',
+        destination: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdGQNmUGUbGXgPQAAqIAbxj6JYeLo0ca8ScL5TO0-XiJBgSPw/formResponse',
+        basePath: false
+      },
+      {
+        source: '/api/budget_form',
+        destination: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSf-Up_irXDJKFfL1b8k3_G9NalEu1TZAonIyjYDEqRrYU-Cow/formResponse',
+        basePath: false
+      },
+    ]
+  },
 };
--- src/util/googleFormsAPI.ts
+++ src/util/googleFormsAPI.ts
@@ -1,19 +1,25 @@
 export const googleFormsAPI = {
   sendContact: async (data: FormData): Promise<void> => {
-    const response = await fetch('/api/contact_form', {
-      method: 'POST',
-      body: data
-    })
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_CONTACTFORM_URL as string,
+      {
+        method: 'POST',
+        body: data
+      }
+    )
     if (!response.ok) {
       throw new Error('Não foi possível enviar sua mensagem, tente novamente')
     }
   },
 
   sendBudget: async (data: FormData): Promise<void> => {
-    const response = await fetch('/api/budget_form', {
-      method: 'POST',
-      body: data
-    })
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_BUDGETFORM_URL as string,
+      {
+        method: 'POST',
+        body: data
+      }
+    )
     if (!response.ok) {
       throw new Error('Não foi possível enviar seu orçamento, tente novamente')
     }
